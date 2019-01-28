8e931b6b9e6500b36ae8416d92e7c6bae81e9f30
dongwang4 <dongwang4@iflytek.com>
2019-01-28 11:31:57 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+        name='pysafelogging',     # 包名字
+        version='0.1',   # 包版本
+        description='Safe logging for python',   # 简单描述
+        url='https://github.com/vovict/pysafelogging',      # 包的主页
+        packages=find_packages(),                 # 包
+)
